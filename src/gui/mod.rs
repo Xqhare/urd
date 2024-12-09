1144ef71bde585a039a09b2a874f4b417d40b5a4
Xqhare <xqhare@web.de>
2024-12-09 19:27:06 +0100

    feat: expanded layout considerably

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,10 +1,17 @@
+use std::{str::FromStr, sync::{atomic::AtomicBool, Arc}};
+
 use eframe::{
     egui::{CentralPanel, Ui},
     epaint::Vec2,
     *,
 };
-use egui::{ScrollArea, SidePanel, TextEdit};
+use egui::{FontId, ScrollArea, SidePanel, TextEdit, TopBottomPanel};
 use crate::settings::Settings;
+
+mod settings;
+mod about;
+mod licenses;
+mod help;
 
 const APP_NAME: &str = "Urd";
 
@@ -12,6 +19,9 @@ pub struct UrdState {
     settings: Settings,
     current_journal_entry: String,
     current_journal_entry_title: String,
+    show_about_viewport: Arc<AtomicBool>,
+    show_licenses_viewport: Arc<AtomicBool>,
+    show_help_viewport: Arc<AtomicBool>,
 }
 
 impl Default for UrdState {
@@ -20,6 +30,10 @@ impl Default for UrdState {
             settings: Settings::default(),
             current_journal_entry: String::new(),
             current_journal_entry_title: String::new(),
+            // default false
+            show_about_viewport: Arc::new(AtomicBool::new(false)),
+            show_licenses_viewport: Arc::new(AtomicBool::new(false)),
+            show_help_viewport: Arc::new(AtomicBool::new(false)),
         }
     }
 }
@@ -29,8 +43,12 @@ impl UrdState {
     pub fn new(settings: Settings) -> Self {
         UrdState {
             settings,
-            current_journal_entry: String::new(),
-            current_journal_entry_title: String::new(),
+            current_journal_entry: String::from_str("Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.").unwrap(),
+            current_journal_entry_title: String::from_str("2024-12-09").unwrap(),
+            // default false
+            show_about_viewport: Arc::new(AtomicBool::new(false)),
+            show_licenses_viewport: Arc::new(AtomicBool::new(false)),
+            show_help_viewport: Arc::new(AtomicBool::new(false)),
         }
     }
 }
@@ -43,25 +61,72 @@ impl App for UrdState {
 
 impl UrdState {
     fn main_page(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        self.main_top_panel(ctx, frame);
         self.main_side_panel(ctx, frame);
         // Remember, central panel last
         self.main_central_panel(ctx, frame);
         
     }
 
+    fn main_top_panel(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        TopBottomPanel::top("top_panel").show(ctx, |ui: &mut Ui| {
+            ui.horizontal(|ui: &mut Ui| {
+                    if ui.button("Settings").clicked() {
+                        self.settings.show_settings_viewport.store(true, std::sync::atomic::Ordering::Relaxed);
+                    }
+                    if ui.button("About").clicked() {
+                        self.show_about_viewport.store(true, std::sync::atomic::Ordering::Relaxed);
+                    }
+                    if ui.button("Licenses").clicked() {
+                        self.show_licenses_viewport.store(true, std::sync::atomic::Ordering::Relaxed);
+                    }
+                    if ui.button("Help").clicked() {
+                        self.show_help_viewport.store(true, std::sync::atomic::Ordering::Relaxed);
+                    }
+            });
+        });
+        if self.settings.show_settings_viewport.load(std::sync::atomic::Ordering::Relaxed) {
+            self.settings_viewport_startup(ctx);
+        }
+        if self.show_about_viewport.load(std::sync::atomic::Ordering::Relaxed) {
+            self.about_viewport_startup(ctx);
+        }
+        if self.show_licenses_viewport.load(std::sync::atomic::Ordering::Relaxed) {
+            self.licenses_viewport_startup(ctx);
+        }
+        if self.show_help_viewport.load(std::sync::atomic::Ordering::Relaxed) {
+            self.help_viewport_startup(ctx);
+        }
+    }
+
     fn main_side_panel(&mut self, ctx: &egui::Context, frame: &mut Frame) {
-        SidePanel::left("entry_browser").default_width(self.settings.side_panel_width).show(ctx, |ui: &mut Ui| {
+        let font = {
+            if self.settings.font.monospace {
+                FontId::monospace(self.settings.font.size)
+            } else {
+                FontId::proportional(self.settings.font.size)
+            }
+        };
+        SidePanel::left("entry_browser").default_width(self.settings.size.side_panel_width).show(ctx, |ui: &mut Ui| {
             ScrollArea::vertical().show(ui, |ui: &mut Ui| {
-
                 for n in 0..25 {
-
-                    let entry_reaction = ui.group(|ui| {
-                        ui.separator();
-                        let title = ui.label(format!("Entry {}", n));
-                        let body = ui.label("Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
-                    }).response;
-                    if entry_reaction.clicked() {
-                        println!("Entry {} clicked", n);
+                    let entry_reaction = ui.add(|ui: &mut Ui| {
+                        let group = ui.group(|ui: &mut Ui| {
+                            let mut tmp_title_str = format!("Entry {}", n);
+                            let mut tmp_body_str = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.".to_string();
+                            ui.add_enabled(false, |ui: &mut Ui| {
+                                TextEdit::singleline(&mut tmp_title_str).frame(false).desired_width(f32::INFINITY).text_color(self.settings.font.text_colour).font(font.clone()).show(ui).response
+                            });
+                            ui.add_enabled(false, |ui: &mut Ui| {
+                                TextEdit::multiline(&mut tmp_body_str).frame(false).desired_width(f32::INFINITY).text_color(self.settings.font.text_colour).font(font.clone()).show(ui).response
+                            })
+                            
+                        });
+                        group.response
+                    });
+                    // TODO: open this journal entry if clicked
+                    if entry_reaction.interact(egui::Sense::click()).clicked() {
+                        println!("Entry reaction! {} clicked", n);
                     }
                 }
                 ui.separator();
@@ -70,17 +135,59 @@ impl UrdState {
     }
 
     fn main_central_panel(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        let font = {
+            if self.settings.font.monospace {
+                FontId::monospace(self.settings.font.size)
+            } else {
+                FontId::proportional(self.settings.font.size)
+            }
+        };
         CentralPanel::default().show(ctx, |ui: &mut Ui| {
+            self.central_panel_menu(ui);
+            
             ScrollArea::vertical().show(ui, |ui: &mut Ui| {
-                let titel = TextEdit::singleline(&mut self.current_journal_entry_title).desired_width(f32::INFINITY).show(ui);
-                let text_edit = TextEdit::multiline(&mut self.current_journal_entry).min_size(ui.available_size()).show(ui);
+                let title = TextEdit::singleline(&mut self.current_journal_entry_title).desired_width(f32::INFINITY).text_color(self.settings.font.text_colour).font(font.clone()).show(ui);
+                let text_edit = TextEdit::multiline(&mut self.current_journal_entry).desired_width(f32::INFINITY).lock_focus(true).text_color(self.settings.font.text_colour).font(font.clone()).show(ui);
+                if self.settings.continuous_saving {
+                    if title.response.changed() || text_edit.response.changed() {
+                        // TODO: save journal entry
+                        // this saves every frame
+                        println!("testing changed");
+                    }
+                } else {
+                    if title.response.lost_focus() || text_edit.response.lost_focus() {
+                        // TODO: save journal entry
+                        // this saves only if the focus leaves the text box
+                        println!("testing lost focus");
+                    }
+                }
+                
             })
+        });
+    }
+
+    fn central_panel_menu(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui: &mut Ui| {
+            ui.group(|ui: &mut Ui| {
+                ui.horizontal(|ui: &mut Ui| {
+                    ui.label("Text Colour: ");
+                    ui.color_edit_button_srgba(&mut self.settings.font.text_colour);
+                })
+            });
+            ui.group(|ui: &mut Ui| {
+                ui.label("Font Size: ");
+                ui.add(egui::Slider::new(&mut self.settings.font.size, 8.0..=48.0));
+            });
+            ui.group(|ui: &mut Ui| {
+                ui.checkbox(&mut self.settings.font.monospace, "Monospace");
+            });
+            let _ = ui.button("Save");
         });
     }
 }
 
 pub fn gui_startup(settings: Settings) {
-    let size: Vec2 = Vec2 { x: settings.size[0], y: settings.size[1] };
+    let size: Vec2 = Vec2 { x: settings.size.size[0], y: settings.size.size[1] };
     let mut native_options = NativeOptions::default();
     native_options.viewport.inner_size = Option::from(size);
     run_native(
---
+++ src/gui/settings.rs
@@ -0,0 +1,25 @@
+use eframe::egui::{CentralPanel, Context, Ui, ViewportBuilder, ViewportId};
+
+use super::UrdState;
+
+impl UrdState {
+    pub fn settings_viewport_startup(&mut self, ctx: &Context) {
+        if self.settings.show_settings_viewport.load(std::sync::atomic::Ordering::Relaxed) {
+            let show_viewport_pointer = self.settings.show_settings_viewport.clone();
+            ctx.show_viewport_deferred(ViewportId::from_hash_of("settings_viewport"), ViewportBuilder::default().with_title("Settings").with_inner_size([200.0, 400.0]), move |ctx, class| {
+                // TODO: paint the settings in the main window if this fails instead of
+                // panicking
+                assert!(class == eframe::egui::ViewportClass::Deferred);
+                CentralPanel::default().show(ctx, |ui: &mut Ui| {
+                    ui.heading("Settings");
+                });
+                // Close viewport
+                if ctx.input(|i| i.viewport().close_requested()) {
+                    show_viewport_pointer.store(false, std::sync::atomic::Ordering::Relaxed);
+                }
+            });
+        }
+    }
+}
+
+
