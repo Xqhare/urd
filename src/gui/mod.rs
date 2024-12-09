6f2f6f2f2b8ef09d44d923f5554dad0e9739d38f
Xqhare <xqhare@web.de>
2024-12-09 16:04:01 +0100

    feat: implementing the needed layout

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,44 +3,77 @@ use eframe::{
     epaint::Vec2,
     *,
 };
-use egui::{ScrollArea, SidePanel};
+use egui::{ScrollArea, SidePanel, TextEdit};
 use crate::settings::Settings;
 
 const APP_NAME: &str = "Urd";
 
 pub struct UrdState {
     settings: Settings,
-
+    current_journal_entry: String,
+    current_journal_entry_title: String,
 }
 
 impl Default for UrdState {
     fn default() -> Self {
         UrdState {
             settings: Settings::default(),
+            current_journal_entry: String::new(),
+            current_journal_entry_title: String::new(),
         }
     }
 }
 
 impl UrdState {
+    // TODO: if settings have been found, there are probably journal entries to check for
     pub fn new(settings: Settings) -> Self {
         UrdState {
             settings,
+            current_journal_entry: String::new(),
+            current_journal_entry_title: String::new(),
         }
     }
 }
 
 impl App for UrdState {
     fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        self.main_page(ctx, frame);
+    }
+}
+
+impl UrdState {
+    fn main_page(&mut self, ctx: &egui::Context, frame: &mut Frame) {
+        self.main_side_panel(ctx, frame);
+        // Remember, central panel last
+        self.main_central_panel(ctx, frame);
+        
+    }
+
+    fn main_side_panel(&mut self, ctx: &egui::Context, frame: &mut Frame) {
         SidePanel::left("entry_browser").default_width(self.settings.side_panel_width).show(ctx, |ui: &mut Ui| {
             ScrollArea::vertical().show(ui, |ui: &mut Ui| {
-                ui.label("Hello World from SidePanel!");
-                ui.label("Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
+
+                for n in 0..25 {
+                    ui.separator();
+                    let title = ui.label(format!("Entry {}", n));
+                    if title.clicked() {
+                        println!("{}", format!("Entry {}", n));
+                    }
+                    let body = ui.label("Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
+                    if body.clicked() {
+                        println!("{}", format!("lorem {}", n));
+                    }
+                }
+                ui.separator();
             });
         });
+    }
+
+    fn main_central_panel(&mut self, ctx: &egui::Context, frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ScrollArea::vertical().show(ui, |ui: &mut Ui| {
-                ui.label("Hello World from CentralPanel!");
-                ui.label("Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.");
+                let titel = TextEdit::singleline(&mut self.current_journal_entry_title).desired_width(f32::INFINITY).show(ui);
+                let text_edit = TextEdit::multiline(&mut self.current_journal_entry).min_size(ui.available_size()).show(ui);
             })
         });
     }
