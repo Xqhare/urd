72d2df6d904d165f41b542ebe4ac737b564d3cd0
Xqhare <xqhare@web.de>
2024-12-12 22:45:03 +0100

    feat: split licenses into their own directory

--- src/gui/licenses.rs
+++ src/gui/licenses.rs
@@ -3,33 +3,8 @@ use eframe::egui::{CentralPanel, Context, ScrollArea, Ui, ViewportBuilder, Viewp
 use super::UrdState;
 
 const LICENSES_WINDOW_SIZE: [f32; 2] = [300.0, 500.0];
-const EFRAME_LICENSE: &str = "Copyright (c) 2018-2021 Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
-
-Permission is hereby granted, free of charge, to any
-person obtaining a copy of this software and associated
-documentation files (the \"Software\"), to deal in the
-Software without restriction, including without
-limitation the rights to use, copy, modify, merge,
-publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice
-shall be included in all copies or substantial portions
-of the Software.
-
-THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF
-ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.";
-
-const HORAE_LICENSE: &str = "";
+const EFRAME_LICENSE: &str = include_str!("../../licenses/MIT-Eframe");
+const HORAE_LICENSE: &str = include_str!("../../licenses/MIT-Horae");
 
 impl UrdState {
     pub fn licenses_viewport_startup(&mut self, ctx: &Context) {
@@ -59,6 +34,8 @@ impl UrdState {
                             ui.label(EFRAME_LICENSE);
                             ui.separator();
                             ui.hyperlink_to("Horae", "https://github.com/xqhare/horae");
+                            ui.label("MIT License");
+                            ui.label(HORAE_LICENSE);
                         });
                     });
                     // Close viewport
