9da067322da8da16b97ef470e68e7605e5641f30
Xqhare <xqhare@web.de>
2024-12-13 17:43:15 +0100

    feat: date_time is now a real date time

--- src/journal_entries.rs
+++ src/journal_entries.rs
@@ -21,27 +21,27 @@ impl Journal {
 pub struct JournalEntry {
     pub title: String,
     pub text: String,
-    pub date_time: String,
+    pub date_time: horae::Utc,
 }
 
 impl JournalEntry {
     pub fn new(settings: &Settings) -> Self {
-        let (date_time, date) = {
+        let (date, title) = {
             if let Some(timezone) = settings.timezone {
                 let (date_time, date) = {
                     let mut date_time = horae::Utc::now();
                     date_time.with_timezone(timezone);
-                    (date_time.to_string(), date_time.date().to_string())
+                    (date_time, date_time.date().to_string())
                 };
                 (date_time, date)
             } else {
-                (horae::Utc::now().to_string(), horae::Utc::now().date().to_string())
+                (horae::Utc::now(), horae::Utc::now().date().to_string())
             }
         };
         Self {
-            title: date,
+            title,
             text: String::new(),
-            date_time,
+            date_time: date,
         }
     }
 }
