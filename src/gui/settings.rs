236a53a743c0a4a6a6f89fd4491e5773775e1b99
Xqhare <xqhare@web.de>
2024-12-16 03:36:10 +0100

    feat: reworked error

--- src/gui/settings.rs
+++ src/gui/settings.rs
@@ -2,7 +2,7 @@
 use eframe::egui::{Align, ComboBox, Context, Grid, ScrollArea, SidePanel, Slider, TextEdit, Ui, Widget};
 use horae::TimeZone;
 
-use crate::settings::{self, Settings, MAX_FONT_SIZE, MAX_SIDE_PANEL_WIDTH, MAX_WINDOW_SIZE, MIN_FONT_SIZE};
+use crate::{error::Error, settings::{self, Settings, MAX_FONT_SIZE, MAX_SIDE_PANEL_WIDTH, MAX_WINDOW_SIZE, MIN_FONT_SIZE}};
 
 use super::UrdState;
 
@@ -27,6 +27,7 @@ impl UrdState {
                                         if but.clicked() {
                                             self.settings = self.settings_backup.clone().unwrap();
                                             self.settings.overwrite_window_size = false;
+                                            self.settings.overwrite_side_panel_width = false;
                                             self.settings.show_settings_viewport = false;
                                         };
                                         but
@@ -37,11 +38,12 @@ impl UrdState {
                                         if but.clicked() {
                                             let save = self.settings.save();
                                             if save.is_err() {
-                                                self.show_error = true;
-                                                self.error_message = Some(save.unwrap_err().to_string());
+                                                self.error = Error::new(save.unwrap_err().to_string());
+                                            } else {
+                                                self.settings.overwrite_window_size = false;
+                                                self.settings.overwrite_side_panel_width = false;
+                                                self.settings_backup = Some(self.settings.clone());
                                             }
-                                            self.settings.overwrite_window_size = false;
-                                            self.settings_backup = Some(self.settings.clone());
                                         };
                                         but
                                     });
@@ -50,12 +52,13 @@ impl UrdState {
                                         if but.clicked() {
                                             let save = self.settings.save();
                                             if save.is_err() {
-                                                self.show_error = true;
-                                                self.error_message = Some(save.unwrap_err().to_string());
+                                                self.error = Error::new(save.unwrap_err().to_string());
+                                            } else {
+                                                self.settings.overwrite_window_size = false;
+                                                self.settings.show_settings_viewport = false;
+                                                self.settings.overwrite_side_panel_width = false;
+                                                self.settings_backup = None;
                                             }
-                                            self.settings.overwrite_window_size = false;
-                                            self.settings.show_settings_viewport = false;
-                                            self.settings_backup = None;
                                         };
                                         but
                                     });
@@ -65,8 +68,7 @@ impl UrdState {
                                     self.settings_backup = Some(self.settings.clone());
                                     let save = self.settings.save();
                                     if save.is_err() {
-                                        self.show_error = true;
-                                        self.error_message = Some(save.unwrap_err().to_string());
+                                        self.error = Error::new(save.unwrap_err().to_string());
                                     }
                                 };
                             }).response
@@ -98,8 +100,7 @@ impl UrdState {
                                         let overwrite_width = self.settings.overwrite_window_size_store[0].parse();
                                         let overwrite_height = self.settings.overwrite_window_size_store[1].parse();
                                         if overwrite_width.is_err() || overwrite_height.is_err() {
-                                            self.show_error = true;
-                                            self.error_message = Some("Invalid window size - Not a number".to_string());
+                                            self.error = Error::new("Invalid window size - Not a number".to_string());
                                             return;
                                         } else {
                                             self.settings.size.size[0] = overwrite_width.unwrap();
@@ -121,8 +122,7 @@ impl UrdState {
                                     if self.settings.overwrite_side_panel_width {
                                         let overwrite_panel_width = self.settings.overwrite_side_panel_width_store.parse();
                                         if overwrite_panel_width.is_err() {
-                                            self.show_error = true;
-                                            self.error_message = Some("Invalid side panel size - Not a number".to_string());
+                                            self.error = Error::new("Invalid side panel size - Not a number".to_string());
                                             return;
                                         } else {
                                             self.settings.size.side_panel_width = overwrite_panel_width.unwrap();
@@ -143,8 +143,7 @@ impl UrdState {
                                         if ui.color_edit_button_srgba(&mut self.settings.font.text_colour).changed() {
                                         let save = self.settings.save();
                                         if save.is_err() {
-                                            self.show_error = true;
-                                            self.error_message = Some(save.unwrap_err().to_string());
+                                            self.error = Error::new(save.unwrap_err().to_string());
                                         }
                                     };
                                     });
@@ -169,51 +168,48 @@ impl UrdState {
 
                         ui.add(|ui: &mut Ui| {
                             ui.group(|ui: &mut Ui| {
-                                let pw_set = if self.settings.password == "" { false } else { true };
+                                let pw_set = if self.settings.password.password == "" { false } else { true };
                                 ui.label("Security");
                                 Grid::new("pw").num_columns(2).show(ui, |ui: &mut Ui| {
                                     ui.label("Old password: ");
                                     ui.add_enabled(pw_set, |ui: &mut Ui| {
-                                        ui.add(TextEdit::singleline(&mut self.settings.password_input).horizontal_align(Align::Center).password(true))
+                                        ui.add(TextEdit::singleline(&mut self.settings.password.password_input).horizontal_align(Align::Center).password(true))
                                     });
                                     ui.end_row();
 
                                     ui.label("New password: ");
-                                    ui.add(TextEdit::singleline(&mut self.settings.new_password_input[0]).horizontal_align(Align::Center).password(true));
+                                    ui.add(TextEdit::singleline(&mut self.settings.password.new_password_input[0]).horizontal_align(Align::Center).password(true));
                                     ui.end_row();
 
                                     ui.label("Repeat new password: ");
-                                    ui.add(TextEdit::singleline(&mut self.settings.new_password_input[1]).horizontal_align(Align::Center).password(true));
+                                    ui.add(TextEdit::singleline(&mut self.settings.password.new_password_input[1]).horizontal_align(Align::Center).password(true));
                                 });
                                 if ui.button("Set new password").clicked() {
                                     let mut set_pw_is_okay = false;
-                                    if self.settings.new_password_input[0] == self.settings.new_password_input[1] {
+                                    if self.settings.password.new_password_input[0] == self.settings.password.new_password_input[1] {
                                         if pw_set {
-                                            if self.settings.password == self.settings.password_input {
-                                                self.settings.password = self.settings.new_password_input[0].to_string();
+                                            if self.settings.password.password == self.settings.password.password_input {
+                                                self.settings.password.password = self.settings.password.new_password_input[0].to_string();
                                                 set_pw_is_okay = true;
                                             } else {
-                                                self.show_error = true;
-                                                self.error_message = Some("Incorrect old password".to_string());
+                                                self.error = Error::new("Incorrect old password".to_string());
                                             }
                                         } else {
-                                            self.settings.password = self.settings.new_password_input[0].to_string();
+                                            self.settings.password.password = self.settings.password.new_password_input[0].to_string();
                                             set_pw_is_okay = true;
                                         }
                                     } else {
-                                        self.show_error = true;
-                                        self.error_message = Some("New password entries do not match".to_string());
+                                        self.error = Error::new("New password entries do not match".to_string());
                                     }
 
                                     if set_pw_is_okay {
-                                        self.settings.password_input = String::new();
-                                        self.settings.new_password_input[0] = String::new();
-                                        self.settings.new_password_input[1] = String::new();
+                                        self.settings.password.password_input = String::new();
+                                        self.settings.password.new_password_input[0] = String::new();
+                                        self.settings.password.new_password_input[1] = String::new();
 
                                         let save = self.settings.save();
                                         if save.is_err() {
-                                            self.show_error = true;
-                                            self.error_message = Some(save.unwrap_err().to_string());
+                                            self.error = Error::new(save.unwrap_err().to_string());
                                         }
                                     }
                                 };
@@ -229,13 +225,12 @@ impl UrdState {
                                         ui.add_space(250.0 / 2.75);
                                     });
                                     ui.add_sized(ui.available_size(), |ui: &mut Ui| {
-                                        ComboBox::from_label("").selected_text(self.settings.timezone.timezone.unwrap().to_string()).show_ui(ui, |ui: &mut Ui| {
+                                        ComboBox::from_label("").selected_text(self.settings.timezone.timezone.to_string()).show_ui(ui, |ui: &mut Ui| {
                                             for tz in self.settings.timezone.all_timezones_str.iter() {
-                                                if ui.selectable_value(&mut self.settings.timezone.timezone, Some(TimeZone::from(tz.clone())), tz.to_string()).clicked() {
+                                                if ui.selectable_value(&mut self.settings.timezone.timezone, TimeZone::from(tz.clone()), tz.to_string()).clicked() {
                                                     let save = self.settings.save();
                                                     if save.is_err() {
-                                                        self.show_error = true;
-                                                        self.error_message = Some(save.unwrap_err().to_string());
+                                                        self.error = Error::new(save.unwrap_err().to_string());
                                                     }
                                                 }
                                             }
