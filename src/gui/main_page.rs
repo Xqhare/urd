23b99cfb198766ed5a51b879e3ce73bec8f0c7bf
Xqhare <xqhare@web.de>
2024-12-17 23:48:05 +0100

    small adjustments

--- src/gui/main_page.rs
+++ src/gui/main_page.rs
@@ -4,7 +4,7 @@ use eframe::{
 };
 use egui::{Align, FontId, ScrollArea, TextEdit};
 
-use crate::settings::{MAX_FONT_SIZE, MIN_FONT_SIZE};
+use crate::{error::Error, journal_entries::{EntryType, Folder, JournalEntry}, settings::{MAX_FONT_SIZE, MIN_FONT_SIZE}};
 
 use super::UrdState;
 
@@ -35,6 +35,7 @@ impl UrdState {
             ui.separator();
             ScrollArea::vertical().show(ui, |ui: &mut Ui| {
                 ui.vertical_centered_justified(|ui: &mut Ui| {
+                    ui.add(TextEdit::singleline(&mut self.journal.current_entry.title).horizontal_align(Align::Center).lock_focus(true));
                     ui.heading(&self.journal.current_entry.title);
                 });
                 ui.separator();
@@ -62,7 +63,10 @@ impl UrdState {
                 ui.horizontal(|ui: &mut Ui| {
                     ui.label("Text Colour: ");
                     if ui.color_edit_button_srgba(&mut self.settings.font.text_colour).changed() {
-                        self.settings.save();
+                        let save = self.settings.save();
+                        if save.is_err() {
+                            self.error = Error::new(save.unwrap_err().to_string());
+                        }
                     };
                 })
             });
@@ -74,13 +78,78 @@ impl UrdState {
                 ui.checkbox(&mut self.settings.font.monospace, "Monospace");
             });
             if ui.button("Save").clicked() {
-                //self.save_journal_entry();
-                let t = self.settings.save();
-                println!("{:?}", t);
+                self.save_journal_entry();
             };
             if ui.button("Delete entry").clicked() {
                 self.delete_journal_entry();
             };
         });
+    }
+
+    fn save_journal_entry(&mut self) {
+        let save = self.settings.save();
+        if save.is_err() {
+            self.error = Error::new(save.unwrap_err().to_string());
+        }
+        // TODO: save journal entry
+        // First search for its position in journal
+        // Then save / overwrite file with same name
+        let (year, month) = {
+            let date = self.journal.current_entry.metadata.get("date").unwrap().into_object().unwrap();
+            let year = date.get("year").unwrap().into_number().unwrap().into_usize().unwrap();
+            let month = date.get("month").unwrap().into_number().unwrap().into_usize().unwrap();
+            (year, month)
+        };
+        let tmp_year_folder = self.journal.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == year.to_string());
+        if tmp_year_folder.is_none() {
+            let mut year_folder = Folder::new(year.to_string());
+            let mut month_folder = Folder::new(month.to_string());
+            let journal_entry_serialized = JournalEntry::deserialize(self.journal.current_entry.text.clone());
+            month_folder.entries.push_front(EntryType::JournalEntry(journal_entry_serialized));
+            year_folder.entries.push_front(EntryType::Folder(month_folder));
+            self.journal.entries.push_front(EntryType::Folder(year_folder));
+        } else {
+            let year_folder = tmp_year_folder.unwrap().get_folder_mut().unwrap();
+            let tmp_month_folder = year_folder.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == month.to_string());
+            if tmp_month_folder.is_none() {
+                let mut month_folder = Folder::new(month.to_string());
+                let journal_entry_serialized = JournalEntry::deserialize(self.journal.current_entry.text.clone());
+                month_folder.entries.push_front(EntryType::JournalEntry(journal_entry_serialized));
+                year_folder.entries.push_front(EntryType::Folder(month_folder));
+            } else {
+                let month_folder = tmp_month_folder.unwrap().get_folder_mut().unwrap();
+                let day_search = month_folder.entries.iter_mut().find(|entry| entry.get_journal_entry().unwrap().title == self.journal.current_entry.title);
+                if day_search.is_none() {
+                    let journal_entry_serialized = JournalEntry::deserialize(self.journal.current_entry.text.clone());
+                    month_folder.entries.push_front(EntryType::JournalEntry(journal_entry_serialized));
+                } else {
+                    // TODO: Fix this
+                    let mut day_folder = day_search.unwrap().get_journal_entry_mut().unwrap();
+                    day_folder = &mut JournalEntry::deserialize(self.journal.current_entry.text.clone());
+                }
+            }
+        }
+    }
+
+    fn delete_journal_entry(&mut self) {
+        let (year, month) = {
+            let date = self.journal.current_entry.metadata.get("date").unwrap().into_object().unwrap();
+            let year = date.get("year").unwrap().into_number().unwrap().into_usize().unwrap();
+            let month = date.get("month").unwrap().into_number().unwrap().into_usize().unwrap();
+            (year, month)
+        };
+        let tmp_year_folder = self.journal.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == year.to_string());
+        if tmp_year_folder.is_some() {
+            let year_folder = tmp_year_folder.unwrap().get_folder_mut().unwrap();
+            let tmp_month_folder = year_folder.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == month.to_string());
+            if tmp_month_folder.is_some() {
+                let month_folder = tmp_month_folder.unwrap().get_folder_mut().unwrap();
+                let actual_entry = month_folder.entries.iter_mut().find(|entry| entry.get_journal_entry().unwrap().title == self.journal.current_entry.title);
+                if actual_entry.is_some() {
+                    actual_entry.unwrap().get_journal_entry_mut().unwrap().text = "".to_string();
+                }
+            }
+        }
+        self.journal.current_entry.text = "".to_string();
     }
 }
