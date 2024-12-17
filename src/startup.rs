4c0b8f3e8b967ac5bb810b2931ba68d6ffa322bc
Xqhare <xqhare@web.de>
2024-12-17 18:47:09 +0100

    feat: made startup appstate more readable / maintainable

--- src/startup.rs
+++ src/startup.rs
@@ -1,24 +1,39 @@
 use std::path::Path;
 
-use crate::{paths::{APP_DIR, SETTINGS_FILE}, settings::Settings};
+use crate::{journal_entries::Journal, paths::{APP_DIR, SETTINGS_FILE}, settings::Settings};
 
+pub struct StartupState {
+    pub settings: Settings,
+    pub journal: Journal,
+}
 
-pub fn startup_appstate() -> Settings {
+pub fn startup_appstate() -> StartupState {
+    let mut settings = Settings::default();
+    let mut journal = Journal::new(&settings);
     if Path::new(APP_DIR).exists() {
-        if Path::new(SETTINGS_FILE).exists() {
-            let out = Settings::deserialize(SETTINGS_FILE);
-            match out {
-                Ok(s) => s,
-                Err(_) => {
-                    Settings::default()
-                }
-            }
-        } else {
-            Settings::default()
-        }
+        settings = startup_settings();
+        // TODO: load journal
     } else {
         // Assume first time startup
         std::fs::create_dir(APP_DIR).unwrap();
+    };
+
+    StartupState {
+        settings,
+        journal,
+    }
+}
+
+fn startup_settings() -> Settings {
+    if Path::new(SETTINGS_FILE).exists() {
+        let out = Settings::deserialize(SETTINGS_FILE);
+        match out {
+            Ok(s) => s,
+            Err(_) => {
+                Settings::default()
+            }
+        }
+    } else {
         Settings::default()
     }
 }
