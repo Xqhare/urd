8e8884efd1519e60f0e5c1785e775c1108bc3b66
Xqhare <xqhare@web.de>
2024-12-22 01:35:39 +0100

    feat: clicking on the month folder header takes you up a level now

--- src/gui/main_page_side_panel.rs
+++ src/gui/main_page_side_panel.rs
@@ -95,6 +95,11 @@ impl UrdState {
                         }
                     }
                     ShowFolder::Month(year, month) => {
+                        ui.vertical_centered_justified(|ui: &mut Ui| {
+                            if ui.heading(format!("{} {}", month_num_to_name(month), year.to_string())).clicked() {
+                                self.go_back_one_level();
+                            }
+                        });
                         ui.separator();
                         let year_folder: &mut Folder = self.journal.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == year.to_string()).expect("Failed to find displayed folder").get_folder_mut().unwrap();
                         let month_folder: &mut Folder = year_folder.entries.iter_mut().find(|entry| entry.get_folder().unwrap().name == month.to_string()).expect("Failed to find displayed folder").get_folder_mut().unwrap();
