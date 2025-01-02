804ccaee0d8f5f47cb20f83a6e02fbf6c28736ee
Xqhare <xqhare@web.de>
2025-01-02 03:36:38 +0100

    doc: expanded error usage doc

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,15 @@ pub struct Error {
 
 impl Error {
     /// Create a new error with a message to be displayed and set `show_error` to true
+    /// 
+    /// param msg: The error message to be displayed
+    /// param msg_ctx: The context of the error
+    ///
+    /// Msg_ctx is shown first, and is supposed to give the error needed context.
+    /// Example:
+    ///
+    /// Instead of only showing "Permission denied", show "Journal cant be read: Permission
+    /// denied".
     ///
     /// Use `Default::default` to create a blank error with `show_error` set to false
     pub fn new<S: Into<String>>(msg: S, msg_ctx: S) -> Self {
