6eb572ffb0e0691f8bce207ad4e5f48ebbff7e99
Xqhare <xqhare@web.de>
2025-01-05 21:08:52 +0100

    layout

--- src/gui/important_days.rs
+++ src/gui/important_days.rs
@@ -1,12 +1,35 @@
 
 use eframe::egui::{CentralPanel, Context, Ui};
 
+use crate::journal_entries::JournalEntry;
+
 use super::UrdState;
 
 impl UrdState {
     pub fn important_days_page(&mut self, ctx: &Context) {
         CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ui.heading("Important Days");
+            for tmp in &self.render.important_day_entries {
+                ui.label(tmp.title.clone());
+                ui.separator();
+            }
         });
+    }
+
+    pub fn construct_important_day_entries(&mut self) {
+        self.render.important_day_entries.clear();
+        for year in &self.journal.entries {
+            debug_assert!(year.is_folder());
+            for month in &year.get_folder().unwrap().entries {
+                debug_assert!(month.is_folder());
+                for entry in &month.get_folder().unwrap().entries {
+                    debug_assert!(entry.is_journal_entry());
+                    let entry = entry.get_journal_entry().unwrap();
+                    if entry.metadata.get("important_day").unwrap().into_boolean().unwrap() {
+                        self.render.important_day_entries.push(entry.clone());
+                    }
+                }
+            }
+        }
     }
 }
